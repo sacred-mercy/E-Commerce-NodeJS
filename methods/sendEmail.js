d0538c137575a6f4555e4c0cf350f2f16816399c
Gaurav <gauravnegi9634@gmail.com>
2023-07-06 14:01:21 +0530

    changePassword module complete

---
+++ methods/sendEmail.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const Mailjet = require("node-mailjet");
+const mailjet = Mailjet.apiConnect(
+    process.env.MJ_APIKEY_PUBLIC,
+    process.env.MJ_APIKEY_PRIVATE
+);
+
+module.exports = (email, subject, textPart, htmlPart, callback) => {
+    const request = mailjet.post("send", { version: "v3.1" }).request({
+        Messages: [
+            {
+                From: {
+                    Email: "nish63348@gmail.com",
+                    Name: "E-Commerce",
+                },
+                To: [
+                    {
+                        Email: email,
+                        Name: "You",
+                    },
+                ],
+                Subject: subject,
+                TextPart: textPart,
+                HTMLPart: htmlPart,
+            },
+        ],
+    });
+    request
+        .then((result) => {
+            console.log(result.body);
+            callback(null, result.body);
+        })
+        .catch((err) => {
+            console.log(err.statusCode);
+            console.log(err.originalMessage);
+            callback(err, null);
+        });
+};
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const fs = require("fs");
+require("dotenv").config();
 const session = require("express-session");
+
+const sendEmail = require("./methods/sendEmail");
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -43,6 +47,13 @@ app.route("/login")
             const users = JSON.parse(data);
             for (let user of users) {
                 if (user.email === email && user.password === password) {
+                    // check if email is verified
+                    if (!user.emailVerification.isEmailVerified) {
+                        res.render("login", {
+                            errorMessage: "Email not verified",
+                        });
+                        return;
+                    }
                     req.session.isLoggedIn = true;
                     req.session.name = user.name;
                     req.session.email = email;
@@ -65,7 +76,10 @@ app.route("/signUp")
             mobile,
             email,
             password,
-            isEmailVerified: false,
+            emailVerification: {
+                isEmailVerified: false,
+                verificationCode: Date.now(),
+            },
         };
 
         fs.readFile("database/users.json", (err, data) => {
@@ -86,20 +100,39 @@ app.route("/signUp")
                 }
             }
 
-            users.push(user);
-            fs.writeFile(
-                "database/users.json",
-                JSON.stringify(users),
-                (err) => {
-                    if (err) {
-                        res.render("login", {
-                            errorMessage: "Invalid email or password",
-                        });
-                    }
-                    console.log("Data written to file");
-                    res.redirect("/login");
+            // Send email verification link
+            const subject = "Email Verification";
+            const textPart = `Greetings from E-commerce.`;
+            const htmlPart =
+                "<h3>Please verify your email by clicking on the link below</h3>" +
+                '<br/><a href="http://localhost:' +
+                process.env.PORT +
+                "/verifyEmail?token=" +
+                token +
+                '">Verify</a>';
+            sendEmail(email, subject, textPart, htmlPart, (err, data) => {
+                if (err) {
+                    res.render("signUp", {
+                        errorMessage: "Invalid email or password",
+                    });
+                    return;
                 }
-            );
+                console.log(data);
+                users.push(user);
+                fs.writeFile(
+                    "database/users.json",
+                    JSON.stringify(users),
+                    (err) => {
+                        if (err) {
+                            res.render("login", {
+                                errorMessage: "Invalid email or password",
+                            });
+                        }
+                        console.log("Data written to file");
+                        res.redirect("/login");
+                    }
+                );
+            });
         });
     });
 
@@ -118,6 +151,100 @@ app.get("/products", (req, res) => {
     });
 });
 
-app.route("*").get((req, res) => res.render("404"));
+app.get("/verifyEmail", (req, res) => {
+    const token = req.query.token;
+    console.log(token);
+    fs.readFile("database/users.json", (err, data) => {
+        if (err) {
+            res.status(500).send("Internal Server Error");
+            return;
+        }
+        const users = JSON.parse(data);
+        for (let user of users) {
+            if (
+                user.emailVerification.verificationCode.toString() === token &&
+                !user.emailVerification.isEmailVerified
+            ) {
+                user.emailVerification.isEmailVerified = true;
+                fs.writeFile(
+                    "database/users.json",
+                    JSON.stringify(users),
+                    (err) => {
+                        if (err) {
+                            res.status(500).send("Internal Server Error");
+                            return;
+                        }
+                        req.session.isLoggedIn = true;
+                        req.session.name = user.name;
+                        req.session.email = user.email;
+                        res.redirect("/");
+                    }
+                );
+                return;
+            }
+        }
+        res.status(404).redirect("*");
+    });
+});
+
+app.route("/changePassword")
+    .get((req, res) => {
+        if (!req.session.isLoggedIn) {
+            res.redirect("/login");
+            return;
+        }
+        res.render("changePassword");
+    })
+    .post((req, res) => {
+        const { newPassword, confirmPassword } = req.body;
+        if (newPassword !== confirmPassword) {
+            res.render("changePassword", {
+                errorMessage: "Confirm password do not match",
+            });
+            return;
+        }
+        fs.readFile("database/users.json", (err, data) => {
+            if (err) {
+                res.status(500).send("Internal Server Error");
+                return;
+            }
+            const users = JSON.parse(data);
+            for (let user of users) {
+                if (user.email === req.session.email) {
+                    if (user.password === newPassword) {
+                        res.render("changePassword", {
+                            errorMessage: "New password cannot be same",
+                        });
+                        return;
+                    }
+                    user.password = newPassword;
+                    fs.writeFile(
+                        "database/users.json",
+                        JSON.stringify(users),
+                        (err) => {
+                            if (err) {
+                                res.status(500).send("Internal Server Error");
+                                return;
+                            }
+                            res.redirect("/logout");
+                            // send a email to user that password has been changed
+                            sendEmail(
+                                user.email,
+                                "Password Changed",
+                                "You have just changed your Password",
+                                ""
+                            );
+                        }
+                    );
+                    return;
+                }
+            }
+        });
+    });
+
+app.route("/forgotPassword")
+    .get((req, res) => res.render("forgotPassword"))
+
+app.route("*").get((req, res) => res.sendFile(__dirname + "/public/404.html"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
